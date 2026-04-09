043cf9322ea06abf9e63039366f8dfbfcbb29999
mavubsa3-dev <mavubsa3@gmail.com>
2026-04-09 23:53:51 +0900

    Service, Repository 변수 선언

--- src/main/java/com/example/schedule/controller/ScheduleController.java
+++ src/main/java/com/example/schedule/controller/ScheduleController.java
@@ -1,9 +1,11 @@
 package com.example.schedule.controller;
 
+import com.example.schedule.service.ScheduleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class ScheduleController {
+    private final ScheduleService scheduleService;
 }
--- src/main/java/com/example/schedule/service/ScheduleService.java
+++ src/main/java/com/example/schedule/service/ScheduleService.java
@@ -1,9 +1,11 @@
 package com.example.schedule.service;
 
+import com.example.schedule.repository.ScheduleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 public class ScheduleService {
+    private final ScheduleRepository scheduleRepository;
 }
