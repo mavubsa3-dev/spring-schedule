7767302afce37f4279f239f0f99fffcef1a18f5a
mavubsa3-dev <mavubsa3@gmail.com>
2026-04-09 23:52:00 +0900

    어노테이션 삽입

--- src/main/java/com/example/schedule/service/ScheduleService.java
+++ src/main/java/com/example/schedule/service/ScheduleService.java
@@ -1,4 +1,9 @@
 package com.example.schedule.service;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class ScheduleService {
 }
