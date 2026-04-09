270cf9b6637441421b91a212ab3f90723905f0d1
mavubsa3-dev <mavubsa3@gmail.com>
2026-04-10 00:35:12 +0900

    service 클래스 일정 생성 리턴 메서드 선언

--- src/main/java/com/example/schedule/controller/ScheduleController.java
+++ src/main/java/com/example/schedule/controller/ScheduleController.java
@@ -1,8 +1,12 @@
 package com.example.schedule.controller;
 
+import com.example.schedule.dto.ScheduleRequest;
+import com.example.schedule.dto.ScheduleResponse;
 import com.example.schedule.service.ScheduleService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,6 +16,10 @@ import org.springframework.web.bind.annotation.RestController;
 public class ScheduleController {
     private final ScheduleService scheduleService;
 
+    @PostMapping
+    public ResponseEntity<ScheduleResponse> createSchedule(@RequestBody ScheduleRequest request){
+        ScheduleResponse result = scheduleService.addSchedule(request);
+    }
 
 
 }
--- src/main/java/com/example/schedule/service/ScheduleService.java
+++ src/main/java/com/example/schedule/service/ScheduleService.java
@@ -1,5 +1,8 @@
 package com.example.schedule.service;
 
+import com.example.schedule.dto.ScheduleRequest;
+import com.example.schedule.dto.ScheduleResponse;
+import com.example.schedule.entity.Schedule;
 import com.example.schedule.repository.ScheduleRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -8,4 +11,17 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ScheduleService {
     private final ScheduleRepository scheduleRepository;
+
+    public ScheduleResponse addSchedule(ScheduleRequest request){
+        Schedule schedule = new Schedule(request.getName(),
+                                         request.getTitle(),
+                                         request.getContent(),
+                                         request.getPassword());
+
+        Schedule savedSchedule = scheduleRepository.save(schedule);
+        return new ScheduleResponse(savedSchedule.getName(),
+                                    savedSchedule.getTitle(),
+                                    savedSchedule.getContent(),
+                                    savedSchedule.getDate());
+    }
 }
