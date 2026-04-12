fd859f29789c609a054670078313e4cef7fc5f3a
mavubsa3-dev <mavubsa3@gmail.com>
2026-04-12 17:04:23 +0900

    댓글 클래스 속성 및 생성자 정의

--- src/main/java/com/example/schedule/entity/ScheduleComment.java
+++ src/main/java/com/example/schedule/entity/ScheduleComment.java
@@ -1,4 +1,31 @@
 package com.example.schedule.entity;
 
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@Entity
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
 public class ScheduleComment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String comment;
+    private String name;
+    private String password;
+
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    private String date;
+
+    public ScheduleComment(String comment, String name, String password){
+        this.comment = comment;
+        this.name = name;
+        this.password = password;
+    }
 }
